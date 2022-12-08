baee9e35dc50523342aacda7e81e9bf890ddb0a1
Leo Ọládimú <82811737+leooladimu@users.noreply.github.com>
2022-12-08 07:22:30 -0500

    1

---
+++ problems/12-is-sorted.js
@@ -0,0 +1,24 @@
+/***********************************************************************
+Write a recursive solution called `isSorted` to determine if the input array
+is sorted in ascending order.
+
+Examples:
+
+***********************************************************************/
+
+
+function isSorted(arr) {
+  if (arr[0] > arr[1]) return false
+  if (arr.length < 2) return true
+  return isSorted(arr.slice(1))
+}
+console.log(isSorted([1, 2, 3, 4, 5])) // true
+console.log(isSorted([1, 2, 4, 3, 5])) // false
+console.log(isSorted([2, 4, 6, 7, 8])) // true
+console.log(isSorted([5, 4, 3, 2, 1])) // false
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = isSorted;
+} catch (e) {
+  module.exports = null;
+}
